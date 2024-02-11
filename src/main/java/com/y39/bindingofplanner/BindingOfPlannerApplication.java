574add97103cef855f7bc1cf41dd02182d423bb2
y39 <y39scode@gmail.com>
2024-02-11 22:14:56 +0900

    init: first setting

---
+++ src/main/java/com/y39/bindingofplanner/BindingOfPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.y39.bindingofplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BindingOfPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BindingOfPlannerApplication.class, args);
+    }
+
+}
