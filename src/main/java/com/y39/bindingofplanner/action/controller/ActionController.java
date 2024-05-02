f00ae28bfb2433e93a407f3edcd9a48eb2853b90
y39 <y39scode@gmail.com>
2024-05-02 23:28:42 +0900

    Feat: Action 엔티티 추가

---
+++ src/main/java/com/y39/bindingofplanner/action/controller/ActionController.java
@@ -0,0 +1,8 @@
+package com.y39.bindingofplanner.action.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ActionController {
+
+}
---
+++ src/main/java/com/y39/bindingofplanner/action/entity/ActionStackHistory.java
@@ -0,0 +1,20 @@
+package com.y39.bindingofplanner.action.entity;
+
+import com.y39.bindingofplanner.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "action_stack_history")
+public class ActionStackHistory extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+
+
+}
