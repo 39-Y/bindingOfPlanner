da417d49102fd7baf4a2798656426ed89824bf55
y39 <y39scode@gmail.com>
2024-06-06 23:08:57 +0900

    Feat: front와 통신하기 위한 테스트 메소드 추가

--- src/main/java/com/y39/bindingofplanner/action/controller/ActionController.java
+++ src/main/java/com/y39/bindingofplanner/action/controller/ActionController.java
@@ -1,8 +1,15 @@
 package com.y39.bindingofplanner.action.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api/v1/action")
 public class ActionController {
+    @GetMapping("/hi")
+    public String getHi(){
+        return "hi";
+    }
 
 }
